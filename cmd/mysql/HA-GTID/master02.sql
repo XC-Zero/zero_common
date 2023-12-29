df3c1841583daa8d2cff0681bdc0fa613571b55d
XCCJM <645171033@qq.com>
2023-12-29 15:30:47 +0800

    [ADD] Mysql HA

---
+++ cmd/mysql/HA-GTID/master02.sql
@@ -0,0 +1,15 @@
+
+CREATE USER if not exists 'copy_root' IDENTIFIED WITH mysql_native_password  BY 'copy_root';
+grant all on *.* to 'copy_root'@'%';
+stop slave;
+
+CHANGE MASTER TO GET_MASTER_PUBLIC_KEY =1;
+flush privileges;
+reset master;
+show master status;
+
+
+change master to master_host='172.165.212.101',master_port=3306,MASTER_USER='copy_root',MASTER_PASSWORD='copy_root',MASTER_AUTO_POSITION=1;
+start slave;
+show slave status;
+
--- cmd/mysql/HA/master01.sql
+++ cmd/mysql/HA/master01.sql
@@ -1,15 +1,16 @@
-CREATE USER 'copy_root' IDENTIFIED BY 'copy_root';
+
+set global server_id =65438;
+CREATE USER if not exists 'copy_root' IDENTIFIED BY 'copy_root';
 grant all on *.* to 'copy_root'@'%';
+stop slave;
+
 CHANGE MASTER TO GET_MASTER_PUBLIC_KEY =1;
 flush privileges;
-
 reset master;
 show master status;
 
-stop slave;
-change master to master_host ='172.16.212.102', master_user ='copy_root',master_password ='copy_root',master_port =3306 ,MASTER_LOG_FILE ='MYSQL_HA_MASTER_02-bin.000001' , MASTER_LOG_POS =157;
+
+change master to master_host ='172.16.212.101', master_user ='copy_root',master_password ='copy_root',master_port =3306 ,MASTER_LOG_FILE ='MYSQL_HA_MASTER_01-bin.000001' , MASTER_LOG_POS =157;
 start slave;
 show slave status;
-show variables like 'gtid%'
 
-set global gtid_mode='ON'
